3b344f94c6c901ef11c4e8b84786239bd6cce7af
Dan Rosenthal <dan.rosenthal@shopify.com>
2019-04-25 11:14:17 -0400

    move comments into index test

--- pages/index.tsx
+++ pages/index.tsx
@@ -12,24 +12,3 @@ export default class Index extends React.PureComponent<{}, never> {
     );
   }
 }
-
-/*
-
-<Board cells onClick />
-<Cell value id onClick/>
-
-*/
-
-/*
-  initializer returns cells data structure given a dimension
-  cells for dimension three would give a 2d array
-  containing three arrays with length three
-  
-  const cells = [
-    [{value: null, id: 1}, {value: null, id: 2}, {value: null, id: 3}],
-    [{value: null, id: 4}, {value: null, id: 5}, {value: null, id: 6}],
-    [{value: null, id: 7}, {value: null, id: 8}, {value: null, id: 9}],
-  ]
-
-  <Board cells={cells} />
-*/
