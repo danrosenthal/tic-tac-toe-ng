458c743cf88638fd535ad5b4a94520408a6c5a54
Dan Rosenthal <dan.rosenthal@shopify.com>
2019-05-18 20:13:03 -0400

    blank slate

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,19 +1,7 @@
 import * as React from "react";
-import Board from "../components/Board";
 
 export default function Index() {
-  const cells = [
-    [{value: null, onClick: noop}, {value: 'O', onClick: noop}, {value: 'O', onClick: noop}],
-    [{value: 'X', onClick: noop}, {value: 'O', onClick: noop}, {value: 'O', onClick: noop}],
-    [{value: 'X', onClick: noop}, {value: 'O', onClick: noop}, {value: 'O', onClick: noop}],
-  ];
-
   return (
-    <>
-      <h1>Tic Tac Toe Next Generation</h1>
-      <Board cells={cells} />
-    </>
+    <p>Hellow world</p>
   );
 }
-
-function noop() {};
