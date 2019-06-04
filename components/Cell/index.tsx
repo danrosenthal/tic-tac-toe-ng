6109123f7ce80c6a21ca85e2b0d8936e0baf6f44
Dan Rosenthal <dan.rosenthal@shopify.com>
2019-06-04 12:09:22 -0400

    try to get styles working

--- components/Cell/index.tsx
+++ components/Cell/index.tsx
@@ -1,12 +1,12 @@
 import * as React from "react";
-import styles from './Cell.scss';
+import * as styles from '../../styles/styles.scss';
 
 export default function Cell() {
-  const cellStyles: React.CSSProperties = {
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'center',
-  }
+  // const cellStyles: React.CSSProperties = {
+  //   display: 'flex',
+  //   alignItems: 'center',
+  //   justifyContent: 'center',
+  // }
 
-  return <div className={styles.Cell} style={cellStyles}>Cell</div>;
+  return <div className={styles.Cell}>Cell</div>;
 }
--- next.config.js
+++ next.config.js
@@ -1,7 +1,12 @@
-const withTypescript = require('@zeit/next-typescript');
-const withSass = require('@zeit/next-sass');
+const withTypescript = require("@zeit/next-typescript");
+const withSass = require("@zeit/next-sass");
 
-module.exports = {
-  withTypescript: withTypescript(),
-  withSass: withSass({cssModules: true}),
-};
+module.exports = withTypescript(
+  withSass({
+    webpack(config, options) {
+      return config;
+    },
+    cssModules: true,
+    sassLoaderOptions: {},
+  })
+);
